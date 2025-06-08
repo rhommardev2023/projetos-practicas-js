d4a1f5471fea55a656a3af0cee0cb676dce7a508
rhommardev2023 <rjmlp02@gmail.com>
2025-06-08 02:59:43 -0700

    se crio o JSON

--- json1.js
+++ json1.js
@@ -1,20 +1,18 @@
-// significado de JSON  javascript object notation
 
+//se transformara en JSON 
 
-// criamos uma funciion que despues sera transformada en um JSON
+let invoice = {
+    name : "Felipe",
+    age : 28 , 
+    products : {
+       0: ["mouse 2xwm", 29.99],
+       1: ["teclado mecanico", 129.99],
+       2: ["monitor", 899.99]
+ }
+}
+
 
 let name = "Felipe";
 let age = 28 ;
 let products = ["mouse 2xwm", "teclado mecanico ", "monitor"];
 let productsvalues = [29.99,129.99,899.99];
-
-//chamada da funcion
-generateInvoice(name, products, productsvalues , age);
-
-function generateInvoice(name, products, productsvalues , age ) {
-    console.log("O comprador é "+ name);
-    console.log(`A idade é ${age}`);
-    console.log("---------------------");
-    console.log("O produto é "+ products[0]);
-    console.log("O valor do produto é "+ productsvalues[0]);
-}
