41bbd2d4f43411015ab7a550c57f65c6b74eef7c
rhommardev2023 <rjmlp02@gmail.com>
2025-06-08 22:12:29 -0700

    2do exemplo de objeto

--- json1.js
+++ json1.js
@@ -1,29 +1,13 @@
 
 //se transformara en JSON 
 
-let invoice = {
-    name : "Felipe",
-    age : 28 , 
-    products : {
-       0: ["mouse 2xwm", 29.99],
-       1: ["teclado mecanico", 129.99],
-       2: ["monitor", 899.99]
-    }
-}
+//exemplo de objeto
+let bichinhos = [
+  { "nome": "Bolinha", "cor": "amarelo", "tipo": "pato" },
+  { "nome": "Fofinho", "cor": "marrom", "tipo": "coelho" }
+];
+let bichinhoEscolhido = bichinhos[0];
+console.log("O " + bichinhoEscolhido.nome + " é " + bichinhoEscolhido.cor + "!");
 
-
-// se chama a funcao usando o json criado
-generateInvoice(invoice)
-
-function generateInvoice(invoice){
-	console.log(`O comprador é ${invoice.name}`)
-    console.log(`A idade é ${invoice.age}`)
-    console.log("------------")
-    
-    for(let index in invoice.products){
-    	let [productName, productPrice] = invoice.products[index]
-        console.log(`- ${productName}: R$ ${productPrice}`)
-    }
-    
-    
-}
+let listaParaAmigo = JSON.stringify(bichinhos);
+console.log(listaParaAmigo);
