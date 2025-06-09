b57f7e4291ad375fcb8105626455c3ce58901c87
rhommardev2023 <rjmlp02@gmail.com>
2025-06-08 21:20:47 -0700

    chamando uma variavel do json

--- json1.js
+++ json1.js
@@ -8,11 +8,8 @@ let invoice = {
        0: ["mouse 2xwm", 29.99],
        1: ["teclado mecanico", 129.99],
        2: ["monitor", 899.99]
- }
+    }
 }
 
-
-let name = "Felipe";
-let age = 28 ;
-let products = ["mouse 2xwm", "teclado mecanico ", "monitor"];
-let productsvalues = [29.99,129.99,899.99];
+//chamando uma variavel do  JSON
+console.log(invoice.name)
