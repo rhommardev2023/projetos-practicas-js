f01c1be9b17155413e13c13d84f69e9419fb13b9
rhommardev2023 <rjmlp02@gmail.com>
2025-06-15 02:03:38 -0700

    comentario na linha 11

--- geracaoDeBiomas.js
+++ geracaoDeBiomas.js
@@ -8,6 +8,7 @@ let minerais = ["  Carvao ", "Ferro ", "  Diamante ", "  Pedra "];
 // Loop para cada golpe, de 1 até a quantidade informada
 for (let i = 1; i <= quantidadeGolpes; i++) {
   // Calcula o índice do mineral usando o operador de módulo (%) para garantir que o índice esteja dentro do tamanho do array
+ //foi editada a linha 11 sustraindo 1 do i para começar do zero
   let minaIndex = (i-1) % minerais.length;
   
   // TODO: Agora exiba o índice i, concatene com o caractere ":", após, concatene com tipo de minerais[minaIndex]:
