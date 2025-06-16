5cf36738c177b29f01ab7ad8f534c0d7ccae877a
rhommardev2023 <rjmlp02@gmail.com>
2025-06-15 21:44:52 -0700

    solucao desafio gerenciamento de items magicos

--- gerenciamentoDeItensMágicos.js
+++ gerenciamentoDeItensMágicos.js
@@ -1,9 +1,14 @@
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
 // Definição da classe ItemMagico
 class ItemMagico {
-//TODO: Crie adequadamente um construtor que receba todos os atributos referente ao item mágico:
-  constructor(  ) {
-
-
+  //TODO: Crie adequadamente um construtor que receba todos os atributos referente ao item mágico:
+  constructor(tipo, dano, resistencia) {
+    this.tipo = tipo;
+    this.dano = dano;
+    this.resistencia = resistencia;
   }
 
   calcularDano() {
@@ -12,15 +17,19 @@ class ItemMagico {
 }
 
 // Solicita ao usuário para digitar o tipo do item mágico, o dano no item e a resistência do item
-const tipoItem = gets();
-const danoItem = parseInt(gets());
-const resistenciaItem = parseInt(gets());
+const tipoItem = "espada";
+const danoItem = 200;
+const resistenciaItem = 300;
 
 //TODO: Crie o de um objeto ItemMagico personalizado com base no tipo escolhido
 
+//TODO: Crie o de um objeto ItemMagico personalizado com base no tipo escolhido
+
+let itemPersonalizado = new ItemMagico(tipoItem, danoItem, resistenciaItem);
+
 // TODO: Imprima os atributos do item personalizado
-console.log("Tipo: " + );
-console.log("Dano: " + );
+console.log("Tipo: " + itemPersonalizado.tipo);
+console.log("Dano: " + itemPersonalizado.dano);
 console.log("Resistencia: " + itemPersonalizado.resistencia);
 
 // Calcula e imprime o dano causado pelo item personalizado em um combate simulado
